14620ab197fc3333a4e8e414726b2f3d28938f0b
Fermin Blanco <luillyfe89@gmail.com>
2020-09-21 11:34:02 -0500

    feature: basic layout system

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-console.log('there we go');
+app()
+
+function app() {
+    const event1 = getComponent('events')
+}
+
+function getComponent(parentId) {
+    const event = document.createElement("section");
+    event.setAttribute('class', 'card event')
+    const main = document.getElementById(parentId);
+    main.appendChild(event);
+}
